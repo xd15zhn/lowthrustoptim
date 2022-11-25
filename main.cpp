6dec71fe35b0ab499cb51880fcbb70b28c7d513d
xd15zhn <yz37zhn@163.com>
2022-11-25 21:43:19 +0800

    11.25 2141 laptop

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ public:
         int dur2 = 22464 - 15000/PI * atan(solution[17]*solution[17]);
         /*第一段发动机工作，为俯仰角和方位角分别设置三次函数*/
         cubic1->Set(solution[0], solution[4], solution[8], solution[12]);
-        cubic2->Set(0*solution[1], 1e-6*solution[5], 1e-3*solution[9], 1e-3*solution[13]);
+        cubic2->Set(1e-3*solution[1], 1e-3*solution[5], solution[9], solution[13]);
         solver._inTheta->Set_Function(cubic1);
         solver._inPhi->Set_Function(cubic2);
         solver._cnstF->Set_OutValue(USV_F);
@@ -61,7 +61,7 @@ public:
             solver._sim1.Simulate_OneStep();
         /*第三段发动机工作，为俯仰角和方位角分别设置新的三次函数*/
         cubic1->Set(solution[2], solution[6], solution[10], solution[14]);
-        cubic2->Set(0*solution[3], 1e-6*solution[7], 1e-3*solution[11], 1e-3*solution[15]);
+        cubic2->Set(1e-3*solution[3], 1e-3*solution[7], solution[11], solution[15]);
         solver._inTheta->Set_Function(cubic1);
         solver._inPhi->Set_Function(cubic2);
         solver._cnstF->Set_OutValue(USV_F);
@@ -71,7 +71,7 @@ public:
         Vector3d errorr = Vector3d(solver._mssr->Get_OutValue()) - tgtr;
         Vector3d errorv = Vector3d(solver._mssv->Get_OutValue()) - tgtv;
         double err = errorr._x*errorr._x + errorr._y*errorr._y + errorr._z*errorr._z;
-        err += 5*(errorv._x*errorv._x + errorv._y*errorv._y + errorv._z*errorv._z);
+        err += 1*(errorv._x*errorv._x + errorv._y*errorv._y + errorv._z*errorv._z);
         err += (dur1 + 22464 - dur2) * 1e-2;
         return err;
     };
@@ -89,19 +89,29 @@ int main(void) {
     Differential_Evolution alg(ans, 10);
     alg.Set_CostFunction(func1);
     alg.Push_Solvsion(vecdble{
-        -0.333662, -1.27126, 0.414759, -0.493854, 0.47648, 1.45296,
-        0.434218, 0.827921, -0.322548, -1.07495, 0.037754, 0.0117591,
-        -1.36123, -0.639763, -0.25881, 0.156407, -0.947391, -0.467629,
+        0.287717, 2.77068, 1.31148, 1.01602, -0.610206, 1.1287,
+        0.482372, 2.97458, -0.75428, 2.42673, 0.91558, 0.618227,
+        -1.53584, -0.00251363, -1.56059, -1.15623, 0.97752, -0.484669,
     });
     alg.Push_Solvsion(vecdble{
-        0.301404, 1.01999, -0.739759, -0.528361, -1.21051, 0.757727,
-        0.427785, -0.712889, 0.312796, 0.338843, 0.382965, -0.420163,
-        -1.51195, -0.757546, 1.3516, -2.18992, 1.07293, -0.568964,
+        0.327971, 2.81796, -0.523281, -0.543126, -1.17108, 1.62028,
+        0.510948, -0.712889, 0.312796, -0.226293, 0.545372, -0.420163,
+        -1.51195, -0.757546, 0.773741, -1.68666, 1.19229, -0.892871,
+    });
+    alg.Push_Solvsion(vecdble{
+        0.287717, 2.77068, 1.31148, 1.01602, -0.610206, 1.1287,
+        0.482372, 2.97458, -0.75428, 2.42673, 0.91558, 0.618227,
+        -1.53584, -0.00251363, -1.56059, -1.15623, 0.97752, -0.484669,
+    });
+    alg.Push_Solvsion(vecdble{
+        0.327971, 2.81796, -0.523281, -0.543126, -1.17108, 2.10536,
+        0.510948, -0.322343, 0.312796, 0.63923, 0.545372, -0.0212892,
+        -1.51195, -0.783517, 0.773741, -1.68666, 1.38849, -0.892871
     });
     cout.precision(6);
     alg.Initialize();
     cout << "\nFirst step finished." << endl;
-    while (1) {
+    while (epoch < 5) {
         cout << "epoch:" << epoch << ", best cost:" << alg.Get_Cost() << ",  best solution:        " << endl;
         alg.Print_Solution();
         alg.Optimize_OneStep();
